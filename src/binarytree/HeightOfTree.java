e6c900602ae5b7be449859caf04f0ad919eca59b
Brijesh patra <brijeshpatra25@gmail.com>
2024-05-16 16:48:05 +0530

    Added files

--- src/binarytree/HeightOfTree.java
+++ src/binarytree/HeightOfTree.java
@@ -29,9 +29,9 @@ public class HeightOfTree {
             return n;
         }
 
-//        public static int heightOfTree(Node root){
-//
-//        }
+        public static int heightOfTree(Node root){
+
+        }
         public static void main(String[] args) {
             int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
             BinaryTree b = new BinaryTree();
